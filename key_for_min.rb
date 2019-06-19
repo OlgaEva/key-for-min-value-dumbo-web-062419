7b2efb424f98eab664dd4873c095d61efe97051e
Olga Rosas <olga.rosas@gmail.com>
2019-06-19 13:37:23 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,10 +3,10 @@
 
 name_hash = {:blake => 500, :ashley => 2, :adam => 1}
 def key_for_min_value(name_hash)
-   lowest_key = 0
-   lowest_value = 0
+   lowest_key = nil
+   lowest_value = nil
    name_hash.each do |k, v|
-     if lowest_value == 0 || v < lowest_value
+     if lowest_value == nil || v < lowest_value
        lowest_value = v
        lowest_key = k
      end
